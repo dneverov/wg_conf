fa326f4647a235f9ade17574c5c7038ff824936c
Denis Neverov <denis.neverov@gmail.com>
2026-08-28 23:55:10 +0300

    Add OptionParser

--- dir.rb
+++ dir.rb
@@ -1,7 +1,23 @@
+require 'optparse'
 require_relative 'lib/file_copier'
+
+# Значение по умолчанию
+options = { period: "0" }
+
+# Настройка парсера ключей
+OptionParser.new do |opts|
+  opts.banner = "Использование: ruby dir.rb [options]"
+
+  opts.on("-p", "--period PERIOD", "Период фильтрации: число дней (например, 3), 0 (сегодня) или all (все файлы)") do |p|
+    options[:period] = p
+  end
+
+  opts.on("-h", "--help", "Показать эту справку") do
+    puts opts
+    exit
+  end
+end.parse!
 
 puts "Запуск синхронизации конфигураций VPN..."
 
-argument = ARGV[0] || "0"
-
-FileCopier.sync!(period_arg: argument)
+FileCopier.sync!(period_arg: options[:period])
