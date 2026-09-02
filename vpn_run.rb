eb47f405e66f2508f0cf05977eb276894dedb363
Denis Neverov <denis.neverov@gmail.com>
2026-09-02 18:53:48 +0300

    Add option to stop connections

--- vpn_run.rb
+++ vpn_run.rb
@@ -1,7 +1,31 @@
+require 'optparse'
 require_relative 'lib/vpn_runner'
 
+options = { stop: false }
+
+OptionParser.new do |opts|
+  opts.banner = "Использование: ruby vpn_run.rb [options] [config_name]"
+
+  # Настраиваем ключ -s для остановки
+  opts.on("-s", "--stop", "Остановить текущее VPN соединение") do
+    options[:stop] = true
+  end
+
+  opts.on("-h", "--help", "Показать эту справку") do
+    puts opts
+    exit
+  end
+end.parse!
+
+# Остаток аргументов после парсинга ключей забираем из ARGV
 config_name = ARGV[0]
 
-puts "Attempting to run VPN with #{config_name}..."
+# Если передан флаг -s, выполняем только остановку и выходим
+if options[:stop]
+  VpnRunner.stop_connections
+  exit 0
+end
+
+puts "Attempting to run VPN with #{config_name || 'latest config'}..."
 
 VpnRunner.run!(config_name)
