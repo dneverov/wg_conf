06939111e9746470a2b333ef643fe074a6d42241
Denis Neverov <denis.neverov@gmail.com>
2026-09-07 12:42:37 +0300

    Add verbose option to vpn_check.rb

--- vpn_check.rb
+++ vpn_check.rb
@@ -1,5 +1,35 @@
+require 'optparse'
 require_relative 'lib/vpn_inspector'
 
+options = { verbose: false }
+
+OptionParser.new do |opts|
+  opts.banner = "Использование: ruby vpn_check.rb [options]"
+
+  opts.on("-v", "--verbose", "Показать детальный диагностический отчет") do
+    options[:verbose] = true
+  end
+
+  opts.on("-h", "--help", "Показать эту справку") do
+    puts opts
+    exit
+  end
+end.parse!
+
+# Если запрошен подробный режим, выводим полную карту параметров
+if options[:verbose]
+  status = VpnInspector.detailed_status
+
+  puts "=== ДИАГНОСТИКА VPN СОЕДИНЕНИЯ ==="
+  puts "Шаблон сервиса systemd : #{status[:vpn_service]}*"
+  puts "Статус службы systemd  : #{status[:service_active] ? 'АКТИВЕН' : 'ВЫКЛЮЧЕН / НЕ НАЙДЕН'}"
+  puts "Сетевой интерфейс      : #{status[:interface_name] || 'НЕ ПОДНЯТ'}"
+  puts "Хост проверки трафика  : #{status[:ping_host]}"
+  puts "Прохождение пинга      : #{status[:ping_successful] ? 'УСПЕШНО' : 'СБОЙ / БЛОКИРОВКА ТСПУ'}"
+  puts "=" * 34
+end
+
+# Финальный лаконичный вывод для совместимости
 print "Проверка VPN-соединения... "
 
 if VpnInspector.connection_active?
